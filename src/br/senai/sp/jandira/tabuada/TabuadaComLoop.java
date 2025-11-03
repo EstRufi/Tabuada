753206b82c402e229c2d030baa0b35f725c7367a
qwe <qwe@cfp127aluno.local>
2025-11-03 17:04:54 -0300

    quase terminando o tabuada

---
+++ src/br/senai/sp/jandira/tabuada/TabuadaComLoop.java
@@ -0,0 +1,16 @@
+package br.senai.sp.jandira.tabuada;
+
+import br.senai.sp.jandira.tabuada.model.TabuadaComLoopApp;
+
+public class TabuadaComLoop {
+
+    public static void main(String[] args) {
+
+        System.out.println("Vamos começar a calcular");
+        System.out.println("---------------");
+
+        TabuadaComLoopApp tabuada = new TabuadaComLoopApp();
+        tabuada.receberDados();
+    }
+
+}
---
+++ src/br/senai/sp/jandira/tabuada/model/TabuadaComLoopApp.java
@@ -0,0 +1,61 @@
+package br.senai.sp.jandira.tabuada.model;
+import java.util.Scanner;
+
+public class TabuadaComLoopApp {
+
+    public int multiplicando;
+    public int multiplicadorInicial;
+    public int multiplicadorFinal;
+    public String[] calcular;
+
+
+    public void receberDados (){
+        Scanner leitor = new Scanner(System.in);
+
+        System.out.print("Qual número vai ser o multiplicando?");
+        multiplicando = leitor.nextInt();
+
+        System.out.print("Bora lá, agora me fala por qual numero irá inicar a multiplicação?");
+        multiplicadorInicial = leitor.nextInt();
+
+        System.out.print("Por fim, me diga até qual número é para multiplicar?");
+        multiplicadorFinal = leitor.nextInt();
+
+        calcularDados();
+    }
+
+    public void calcularDados(){
+        int apoio = 0;
+        if(multiplicadorFinal < multiplicadorInicial){
+            apoio = multiplicadorFinal;
+            multiplicadorFinal = multiplicadorInicial;
+            multiplicadorInicial = apoio;
+        }
+
+
+        int tamanho = multiplicadorFinal - multiplicadorInicial + 1;
+        calcular = new String[tamanho];
+
+        int i = 0;
+        while(i < tamanho){
+            int produto = multiplicando * multiplicadorInicial;
+            calcular[i] = multiplicando + " X " + multiplicadorInicial + " = " + produto;
+            multiplicadorInicial = multiplicadorInicial + 1;
+
+            i = i + 1;
+        }
+        exibirTabuada();
+    }
+
+    public void exibirTabuada(){
+        System.out.println("O resultado que você pediu da Tabuada !!!");
+
+        int i = 0;
+        while(i < calcular.length){
+            System.out.println(calcular[i]);
+            i++; // quer dizer que é i = i + 1
+        }
+
+    }
+
+}
